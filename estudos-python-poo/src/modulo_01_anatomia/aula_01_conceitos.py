8679b8246d706baef1b51ed67ca67380797d2ad9
Barbara <40583024+barbaraguarino@users.noreply.github.com>
2025-12-03 14:37:10 -0300

    Aprendendo o parâmetro self

--- estudos-python-poo/src/modulo_01_anatomia/aula_01_conceitos.py
+++ estudos-python-poo/src/modulo_01_anatomia/aula_01_conceitos.py
@@ -1,20 +1,15 @@
-# Definição da Classe (O Molde)
 class Caneta:
-    # O Inicializador
-    # self: refere-se ao objeto que acabou de ser criado (falaremos mais dele depois)
-    # cor, marca: são os argumentos que passamos ao criar a caneta
+
     def __init__(self, cor, marca):
-        # Aqui estamos anexando dados ao objeto
         self.cor = cor
         self.marca = marca
-        print(f"Uma nova caneta {marca} {cor} foi criada!")
 
-# Instanciação (Fabricando Objetos)
-# Note que passamos 'Azul' e 'Bic', mas não passamos o self.
-# O Python passa o objeto recém-criado como 'self' automaticamente.
-caneta_1 = Caneta("Azul", "Bic")
-caneta_2 = Caneta("Vermelha", "Faber")
+    def descrever(self):
+        return f"Caneta da marca {self.marca} da cor {self.cor}."
 
-# Acessando os atributos que definimos no __init__
-print(f"Objeto 1: {caneta_1.marca} - {caneta_1.cor}")
-print(f"Objeto 2: {caneta_2.marca} - {caneta_2.cor}")
+if __name__ == '__main__':
+    caneta_1 = Caneta("azul", "Bic")
+    caneta_2 = Caneta("vermelha", "Faber")
+
+    print(caneta_1.descrever())
+    print(caneta_2.descrever())
